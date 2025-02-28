f6a32e13e20768e7310f89802f2365958c67b3b1
Joan Preciado <js.preciadova@gmail.com>
2025-02-28 12:30:01 -0500

    Fix: Update data URL

--- js/master.js
+++ js/master.js
@@ -1,8 +1,8 @@
 const credencial =
-  "https://docs.google.com/spreadsheets/d/e/2PACX-1vRIk9-wyk1WyU_4lTWjLucuXSHAYXGsgu0gAhynPQXccaPfe6ANyGtcxPf1jcLDX0dA2Eb9OiE8N5T5/pub?gid=0&single=true&output=csv";
+  "https://docs.google.com/spreadsheets/d/e/2PACX-1vRT2RpVUyWfqT6zUMlMrNVEpzR4SBoG2BwFRKxYBqwIzXy9dq-nxQti4Ob13rBBBg/pub?gid=0&single=true&output=csv";
 
 const cursos =
-  "https://docs.google.com/spreadsheets/d/e/2PACX-1vRIk9-wyk1WyU_4lTWjLucuXSHAYXGsgu0gAhynPQXccaPfe6ANyGtcxPf1jcLDX0dA2Eb9OiE8N5T5/pub?gid=1938613270&single=true&output=csv";
+  "https://docs.google.com/spreadsheets/d/e/2PACX-1vRT2RpVUyWfqT6zUMlMrNVEpzR4SBoG2BwFRKxYBqwIzXy9dq-nxQti4Ob13rBBBg/pub?gid=431055798&single=true&output=csv";
 
 window.addEventListener("DOMContentLoaded", init);
 
